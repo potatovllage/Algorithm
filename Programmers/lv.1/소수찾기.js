3f3bc95d3d8f64d2f7bfb9f658545012ae2aabea
Yeong joon <bluehome8626@naver.com>
2023-04-24 23:50:18 +0900

    [Programmers] 소수찾기

---
+++ Programmers/lv.1/소수찾기.js
@@ -0,0 +1,20 @@
+function solution(n) {
+  let answer = 0;
+  const arr = new Array(n + 1).fill(true);
+
+  for (let i = 2; i <= n / 2; ++i) {
+    if (arr[i] === false) {
+      continue;
+    }
+    for (let k = i * i; k <= n; k += i) {
+      arr[k] = false;
+    }
+  }
+
+  for (let i = 2; i <= n; ++i) {
+    if (arr[i] === true) {
+      answer++;
+    }
+  }
+  return answer;
+}
